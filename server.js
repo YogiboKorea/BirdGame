e69f0a01388ea5ea032aeb0c1d081a67335012b2
YogiboKorea <yogico.design@gmail.com>
2025-04-22 14:38:31 +0900

    birds

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const path    = require('path');
+
+const app  = express();
+const PORT = process.env.PORT || 3000;
+
+// public 폴더 안의 index.html을 / 요청에 자동으로 서빙합니다.
+app.use(express.static(path.join(__dirname, 'public'), {
+  index: 'index.html'
+}));
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
